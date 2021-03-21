c16a54341523a0bc14a9ca4ad8b72ad9c8f8b669
unknown <k.burke803@gmail.com>
2021-03-20 21:15:49 -0500

    Added files from midterm demo to github

---
+++ sql/dummy.sql
@@ -0,0 +1,25 @@
+use puncys_store_1;
+
+insert into job_presets (type, garment_price) values ("Skirt-XS",  5000);
+insert into job_presets (type, garment_price) values ("Skirt-S",  5000);
+insert into job_presets (type, garment_price) values ("Skirt-M",  5000);
+insert into job_presets (type, garment_price) values ("Skirt-L",  5000);
+insert into job_presets (type, garment_price) values ("Skirt-XL",  5000);
+
+insert into job_presets (type, garment_price) values ("Pants-XS",  5000);
+insert into job_presets (type, garment_price) values ("Pants-S",  5000);
+insert into job_presets (type, garment_price) values ("Pants-M",  5000);
+insert into job_presets (type, garment_price) values ("Pants-L",  5000);
+insert into job_presets (type, garment_price) values ("Pants-XL",  5000);
+
+insert into job_presets (type, garment_price) values ("Shorts-XS",  5000);
+insert into job_presets (type, garment_price) values ("Shorts-S",  5000);
+insert into job_presets (type, garment_price) values ("Shorts-M",  5000);
+insert into job_presets (type, garment_price) values ("Shorts-L",  5000);
+insert into job_presets (type, garment_price) values ("Shorts-XL",  5000);
+
+insert into job_presets (type, garment_price) values ("Shirt-XS",  5000);
+insert into job_presets (type, garment_price) values ("Shirt-S",  5000);
+insert into job_presets (type, garment_price) values ("Shirt-M",  5000);
+insert into job_presets (type, garment_price) values ("Shirt-L",  5000);
+insert into job_presets (type, garment_price) values ("Shirt-XL",  5000);
