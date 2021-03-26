a5c172a504ce7153b79b4fc27ab940fcbdf4b8a5
unknown <k.burke803@gmail.com>
2021-03-26 03:15:47 -0500

    Fixed order form, login, register user functionality, home page still ugly, added flashed messages to home page.

--- application/app.py
+++ application/app.py
@@ -31,6 +31,38 @@ def logoutUser():
     session['DOB'] = None
     session['clearance'] = None
 
+def compare_dates(date1: str, date2: str): #Expected input: Two dates in the format YYYY-MM-DD
+    # Returns -1 if date2 happens before date1, 0 if both dates are the same and 1 if date1 happens before date2
+    # compare_dates("March 3rd 2021", "March 12th 2021") ==> 1
+    # compare_dates("July 3rd 2021", "March 12th 2021") ==> -1 
+    # compare_dates("March 12th 2021", "March 12th 2021") ==> 0 
+    date1 = list(map(int, date1.split("-")))
+    date2 = list(map(int, date2.split("-")))
+    if (date1[0] < date2[0]):
+        # If the year in date 1 is less than the year in date 2
+        return 1
+    elif (date1[0] > date2[0]):
+        # If the year in date 2 is less than the year in date 1
+        return -1
+    # Both years are equal, so check months
+    elif (date1[1] < date2[1]):
+        # If the month in date 1 is less than the month in date 2
+        return 1
+    elif (date1[1] > date2[1]):
+        # If the month in date 2 is less than the month in date 1
+        return -1
+    # Both months are equal, so check days
+    elif (date1[2] < date2[2]):
+        # If the day in date 1 is less than the day in date 2
+        return 1
+    elif (date1[2] > date2[2]):
+        # If the day in date 2 is less than the day in date 1
+        return -1
+    # Both dates are equal, return 0
+    else: 
+        return 0
+
+
 
 @app.route("/", methods = ['POST', 'GET']) # Login page. POST returns the form, POST attempts to login the
                                            # user with the information provided 
@@ -63,6 +95,28 @@ def login():
     else:
         return make_response("Invalid Request Method.", 400)
     pass
+
+@app.route("/view_orders")
+def view_orders():
+    if (session['clearance'] == 0):
+            pass
+    else:
+        flash("You do not have authority to view this page.")
+        return redirect(url_for('home'))
+    orders = query("select * from orders;").fetchall()
+    return render_template("view_orders.html", orders = orders)
+
+@app.route("/view_orders/<user_id>")
+def view_all_orders(user_id):
+    # This is the view for a customer viewing their specific orders
+    if not((session['user_id'] == user_id)):
+        if session['clearance'] == 0:
+            pass
+        else:
+            flash("You do not have authority to view this page.")
+            return redirect(url_for('home'))
+    orders = query("select * from orders where user_id = {};".format(user_id)).fetchall()
+    return render_template("view_orders.html", orders = orders)
 
 @app.route("/logout")
 def logout():
@@ -134,35 +188,47 @@ def home():
 @app.route("/order", methods = ['POST', 'GET'])
 #@login_required
 def order():
-    """ job_types = query("SELECT type from job_presets;")
-    user_measurements = query("SELECT * from measurements where user_id = {};".format(1)) """
-    form = OrderForm()
-    try:
+    """try:
         if(session['user_id'] == None):
             flash("You must log in to view this page. Please log in below.")
             return redirect(url_for('login'))
     except:
         flash("You must log in to view this page. Please log in below.")
-        return redirect(url_for('login'))
+        return redirect(url_for('login'))"""
+    
+    job_types = query("SELECT type from job_presets;")
+    user_measurements = query("SELECT * from measurements where user_id = {};".format(1)).fetchall()
+    measurements = [(measurement.measurement_id, (str((measurement.name, measurement.job_type)))) for measurement in user_measurements] 
+    form = OrderForm()
+    form.meas_select.choices= measurements
     if request.method == 'POST':
-        user_id = session['user_id']
+        try:
+            user_id = session['user_id']
+        except:
+            user_id = 1
         first_name = form.fname.data
         last_name = form.lname.data
         contact_num = form.tele.data
         addr = form.addr.data
         typ = form.job_type.data
-        measurement_id = 1
+        
         date_placed = datetime.date.today().strftime("%Y-%m-%d")
         est_cost = 5000
+        
+        due_date = form.due_date.data.split("/")
+        due_date = "{}-{}-{}".format(due_date[2], due_date[0],due_date[1])
         providing_fab = form.providing_fab.data
         if form.new_meas.data:
             new_measurement = Measurement(user_id = user_id, job_type = typ, name = form.meas_name.data, length = form.length.data, waist= form.waist.data,\
                 hip = form.hip.data, ankle = form.ankle.data, round_leg= form.round_leg.data, round_ankle= form.round_ankle.data, bust = form.bust.data,\
                     sleeve = form.sleeve.data, bicep = form.bicep.data, armhole = form.armhole.data, neck = form.neck.data, shoulder = form.shoulder.data,\
                          across_back = form.across_back.data, bust_point= form.bust_point.data, round_knee = form.round_knee.data)
+            measurement_id = query("select max(measurement_id) from measurements;").first()['max(measurement_id)']
+        else:
+            measurement_id = form.meas_select.data
         new_order = Order(user_id = user_id, first_name = first_name, last_name = last_name, contact_num = contact_num, delivery_address=addr, \
                             typ = typ, measurement_id=measurement_id, est_cost = est_cost, date_placed = date_placed, \
-                            providing_fabric=form.providing_fab.data)
+                            providing_fabric=form.providing_fab.data, due_date=due_date)
         if(form.need_appt.data):
             return redirect(url_for('appointment'))
         else:
@@ -192,7 +258,7 @@ def appointment():
     if request.method == 'POST':
         pass
     elif request.method == 'GET':
-        return "<h1>APPOINTMENT FORM</h1>"
+        return render_template("appointment.html")
     else:
         return make_response("Invalid Request Method.", 400)
     pass
--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,6 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, DateField, TimeField, SubmitField, BooleanField, SelectField
+from wtforms import StringField, PasswordField, DateField, TimeField, SubmitField, BooleanField, SelectField, Field
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from flask_wtf import FlaskForm
 from .setup import query
@@ -23,60 +23,53 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('submit')
 
 class AppointmentForm(FlaskForm):
-    date = DateField('date', validators = [DataRequired()], render_kw={"placeholder":"Date"})
+    date = DateField('date', validators = [DataRequired()], format="%Y-%m-%d", render_kw={"placeholder":"Date"})
     time = TimeField('time', validators = [DataRequired()], render_kw={"placeholder":"Time"})
     submit = SubmitField('submit')
 
 class OrderForm(FlaskForm):
-    job_types = query("select type from job_presets;")
-    """ def __init__(self):
-    #    
-    #    try:
-    #        user_measurements = query("select * from measurements where user_id = {}".format(self.data['session_id']))
-    #        print("----------------------------------\n")
-    #        print(user_measurements)
-    #        print("----------------------------------\n")
-    #    except Exception as e:
-    #        print("HELLLOOOO")
-    #        print(e) """
+    
+    
+        job_types = query("select type from job_presets;")
+        #user_measurements = query("select * from measurements where user_id = {};".format(user_id))
 
     # || User Data
-    fname      = StringField('fname', validators = [DataRequired()], render_kw={})
-    lname      = StringField('lname', validators = [DataRequired()], render_kw={})
-    tele       = StringField('tele', validators = [DataRequired()], render_kw={})
-    addr       = StringField('addr', validators = [DataRequired()], render_kw={})
-    due_date   = DateField('due-date', validators = [], render_kw={})
+        fname      = StringField('fname', validators = [DataRequired()], render_kw={})
+        lname      = StringField('lname', validators = [DataRequired()], render_kw={})
+        tele       = StringField('tele', validators = [DataRequired()], render_kw={})
+        addr       = StringField('addr', validators = [DataRequired()], render_kw={})
+        due_date   = StringField(id="datepicker", validators = [], render_kw={}, _name="due_date")
 
-    # || Order Measurements
-    length      = StringField('length', validators = [DataRequired()], render_kw={"value":0})
-    hip         = StringField('hip', validators = [DataRequired()], render_kw={"value":0})
-    waist       = StringField('waist', validators = [DataRequired()], render_kw={"value":0})
-    across_back = StringField('across_back', validators = [DataRequired()], render_kw={"value":0})
-    round_leg   = StringField('round_leg', validators = [DataRequired()], render_kw={"value":0})
-    round_knee  = StringField('round_knee', validators = [DataRequired()], render_kw={"value":0})
-    bust        = StringField('bust', validators = [DataRequired()], render_kw={"value":0})
-    ankle       = StringField('ankle', validators = [DataRequired()], render_kw={"value":0})
-    neck        = StringField('neck', validators = [DataRequired()], render_kw={"value":0})
-    shoulder    = StringField('shoulder', validators = [DataRequired()], render_kw={"value":0})
-    sleeve      = StringField('sleeve', validators = [DataRequired()], render_kw={"value":0})
-    bicep       = StringField('bicep', validators = [DataRequired()], render_kw={"value":0})
-    bust_point  = StringField('bust_point', validators = [DataRequired()], render_kw={"value":0})
-    armhole     = StringField('armhole', validators = [DataRequired()], render_kw={"value":0})
-    round_ankle = StringField('round_ankle', validators = [DataRequired()], render_kw={"value":0})
+        # || Order Measurements
+        length      = StringField('length', validators = [DataRequired()], render_kw={"value":0})
+        hip         = StringField('hip', validators = [DataRequired()], render_kw={"value":0})
+        waist       = StringField('waist', validators = [DataRequired()], render_kw={"value":0})
+        across_back = StringField('across_back', validators = [DataRequired()], render_kw={"value":0})
+        round_leg   = StringField('round_leg', validators = [DataRequired()], render_kw={"value":0})
+        round_knee  = StringField('round_knee', validators = [DataRequired()], render_kw={"value":0})
+        bust        = StringField('bust', validators = [DataRequired()], render_kw={"value":0})
+        ankle       = StringField('ankle', validators = [DataRequired()], render_kw={"value":0})
+        neck        = StringField('neck', validators = [DataRequired()], render_kw={"value":0})
+        shoulder    = StringField('shoulder', validators = [DataRequired()], render_kw={"value":0})
+        sleeve      = StringField('sleeve', validators = [DataRequired()], render_kw={"value":0})
+        bicep       = StringField('bicep', validators = [DataRequired()], render_kw={"value":0})
+        bust_point  = StringField('bust_point', validators = [DataRequired()], render_kw={"value":0})
+        armhole     = StringField('armhole', validators = [DataRequired()], render_kw={"value":0})
+        round_ankle = StringField('round_ankle', validators = [DataRequired()], render_kw={"value":0})
 
-    # || Important Booleans
-    need_appt     = BooleanField('need_appt', validators = [], render_kw={"selected-value":1, "default":"checked"})
-    providing_fab = BooleanField('need_appt', validators = [], render_kw={"selected-value":1, "default":"checked"})
-    new_meas      = BooleanField('new_meas', validators = [], render_kw={})
+        # || Important Booleans
+        need_appt     = BooleanField('need_appt', validators = [], render_kw={"selected-value":1, "default":"checked"})
+        providing_fab = BooleanField('need_appt', validators = [], render_kw={"selected-value":1, "default":"checked"})
+        new_meas      = BooleanField('new_meas', validators = [], render_kw={})
 
-    # || Miscellaneous
-    meas_name     = StringField('meas_name', validators = [], render_kw={})
-    meas_select   = SelectField('meas_select', validators = [], render_kw={}, choices= [])
-    job_type      = SelectField('job_type', validators = [DataRequired()], render_kw ={}, choices = (["Select a job type..."] + [job.type for job in job_types]))
-    job_type_text = StringField('job_type_text', validators = [], render_kw = {})
+        # || Miscellaneous
+        meas_name     = StringField(_name = 'meas_name', validators = [], render_kw={})
+        meas_select   = SelectField('meas_select', validators = [], render_kw={})
+        job_type      = SelectField('job_type', validators = [DataRequired()], render_kw ={}, choices = (["Select a job type..."] + [job.type for job in job_types]))
+        job_type_text = StringField('job_type_text', validators = [], render_kw = {})
 
-    # || Submit
-    submit = SubmitField('Submit')
+        # || Submit
+        submit = SubmitField('Submit')
 
     
     
